942daceac25dc48c58c4c1dae49f4a81f5e9f0ab
Esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-03-15 17:36:14 +0000

    jgame

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var inited = false;
 exports.getInit = function() {
-  
+  inited = true;
+};
+exports.makeplayer = function(color, borderradius) {
+  if(inited){
+    console.log(color)
+    const player = document.createElement("div")
+    const style = 
+      "background: "+color+";"+
+      "border-radius: "+borderradius+";"
+
+    player.setAttribute("style", style)
+    
+    document.body.appendChild(player)
+  }
+}
+exports.makebg = function(color) {
+  if(inited){
+    console.log(color)
+    document.body.style.background = color
+  }
 }
